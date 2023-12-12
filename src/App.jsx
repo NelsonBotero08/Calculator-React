fb1b076a9343d3446b251e20a28dc92cca95089b
NelsonBotero1518 <nelsonkennedy2004@gmail.com>
2023-12-12 17:04:12 -0500

    correccion error

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,65 @@
-import { useState } from 'react'
-import './App.css'
-import Buttons from './components/Buttons'
-import Display from './components/Display'
-import ButtonClear from './components/ButtonClear'
-import { evaluate } from 'mathjs';
+import { useState } from "react";
+import "./App.css";
+import Buttons from "./components/Buttons";
+import Display from "./components/Display";
+import ButtonClear from "./components/ButtonClear";
+import { evaluate } from "mathjs";
 
 function App() {
+  const [input, setInput] = useState("");
 
-  const [input, setInput] = useState("")
-
-  const addInput = value => {
-    setInput(input + value)
-  }
+  const addInput = (value) => {
+    setInput(input + value);
+  };
 
   const result = () => {
-    setInput(evaluate(input))
-  }
+    try {
+      if (input.trim() !== "") {
+        setInput(evaluate(input));
+      }
+    } catch (error) {
+      setInput("Error");
+    }
+  };
 
   const clear = () => {
-    setInput("")
-  }
+    setInput("");
+  };
 
   return (
-    <div className='App'>
-      <div className='container'>
-        <Display input={input}/>
-        <div className='file'>
+    <div className="App">
+      <div className="container">
+        <Display input={input} />
+        <div className="file">
           <Buttons addInput={addInput}>7</Buttons>
           <Buttons addInput={addInput}>8</Buttons>
           <Buttons addInput={addInput}>9</Buttons>
           <Buttons addInput={addInput}>/</Buttons>
         </div>
-        <div className='file'>
+        <div className="file">
           <Buttons addInput={addInput}>4</Buttons>
           <Buttons addInput={addInput}>5</Buttons>
           <Buttons addInput={addInput}>6</Buttons>
           <Buttons addInput={addInput}>*</Buttons>
         </div>
-        <div className='file'>
+        <div className="file">
           <Buttons addInput={addInput}>1</Buttons>
           <Buttons addInput={addInput}>2</Buttons>
           <Buttons addInput={addInput}>3</Buttons>
           <Buttons addInput={addInput}>-</Buttons>
         </div>
-        <div className='file'>
+        <div className="file">
           <Buttons addInput={result}>=</Buttons>
           <Buttons addInput={addInput}>0</Buttons>
           <Buttons addInput={addInput}>.</Buttons>
           <Buttons addInput={addInput}>+</Buttons>
         </div>
-        <div className='file'>
-          <ButtonClear clear={clear}/>
+        <div className="file">
+          <ButtonClear clear={clear} />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
